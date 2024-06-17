788f5992802fa442796028ce387f5f0d1cb6cbd7
ShoibDev <sediq1973@gmail.com>
2024-06-17 13:55:53 -0400

    setup with next+supa

---
+++ quick-note/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import {ThemeToggle} from "@/app/components/theme-toggle";
+
+export default function Home() {
+  return (
+    <>
+      <ThemeToggle />
+    </>
+  );
+}
